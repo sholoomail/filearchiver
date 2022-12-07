f3b9c9ee483d95a1cf1de1463e1a4daa07bb938c
CreativeJr <73512563+CreativeJr@users.noreply.github.com>
2022-12-07 20:54:29 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
-bot_token=os.getenv("BOT_TOKEN")
-app_id=int(os.getenv("APP_ID"))
-app_hash=os.getenv("APP_HASH")
-session_dsn=os.getenv("SESSION_NAME")
+bot_token=os.getenv("BOT_TOKEN", "5255621390:AAEiWNN3Zy1BISUj2PhYnlVulR2emKDGYhA")
+app_id=int(os.getenv("APP_ID", "4926633"))
+app_hash=os.getenv("APP_HASH", "be7b1d39ac5116d22701f8b6ac956785")
+session_dsn=os.getenv("SESSION_NAME", "zipfile_bot")
